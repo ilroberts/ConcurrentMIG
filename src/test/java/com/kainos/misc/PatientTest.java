24677b0958c2434def4e1dc1d5a0ad70b6120900
illroberts <iwanr@kainos.com>
2017-10-24 14:16:06 +0100

    extracting patient id using xpath

--- src/test/java/com/kainos/misc/PatientTest.java
+++ src/test/java/com/kainos/misc/PatientTest.java
@@ -15,26 +15,32 @@ import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
-public class AaltoTest  {
+public class PatientTest {
 
     @Test
-    public void testAalto() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException{
+    public void testRetrievePatient() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException {
 
-        File xmlFile = new File("/src/test/resources/mig-patient-trace.xml");
+        File xmlFile = new File("./src/test/resources/4853379371-investigations.xml");
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document document = builder.parse(xmlFile);
+        String expression = "(/Envelope/Body/migResponse/serviceContent/ehrExtractResponsev2-0/patient/id)";
+
+        Optional<Node> patientNode = getNodeStreamFromXpath(document, expression).findFirst();
+        String patientId = patientNode.map(n -> n.getFirstChild().getNodeValue()).orElse("");
+        assertEquals(patientId, "118fd62d-98f4-4b30-b324-99c00b7de212");
+    }
+
+    public Stream<Node> getNodeStreamFromXpath(Document document, String expression) throws XPathExpressionException {
 
         XPath xpath = XPathFactory.newInstance().newXPath();
-        String expression = "(/Envelope/Body/migResponse/serviceContent/extendedTraceResponse/serviceProfiles/values/value)";
         NodeList list = (NodeList) xpath.compile(expression).evaluate(document, XPathConstants.NODESET);
-        Stream<Node> nodeStream = IntStream.range(0, list.getLength()).mapToObj(list::item);
-        nodeStream.forEach(n -> System.out.println(n.getFirstChild().getNodeValue()));
-        assertEquals(list.getLength(), 12);
+        return IntStream.range(0, list.getLength()).mapToObj(list::item);
     }
 }
